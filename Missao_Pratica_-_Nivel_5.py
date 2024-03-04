241b4f9bd4f87b2f1cc4a90b7fbbe2e27ff884bb
Rafael Valverde Dev <rafaelvalverdefonseca@gmail.com>
2024-03-04 12:00:26 -0300

    "Atualização de pastas"

---
+++ Missao_Pratica_-_Nivel_5.py
@@ -0,0 +1,27 @@
+# DESCRIÇÃO DA MISSÃO
+## Implementar um programa para manipular dados de um arquivo texto e visualizá-lo em um histograma e nuvem de palavras
+
+# OBJETIVOS
+## 1º Objetivo
+### Gerar dados de teste
+
+## 2º Objetivo
+### Gravar e recuperar dados de arquivos texto
+
+## 3º Objetivo
+### Manipular os dados para visualizaá-los em um hisograma
+
+## 4º Objetivo
+### Manipular os dados para visulaizá-los e uma nuvem de palavras
+
+## 5º Objetivo
+### Implementar um programa para gerar dados com nomes de pessoas e respectivas pontuações,
+### gravar em um arquivo, recuperar os dados do arquivo, visualiazar os dados das pontuações das pessoas
+### em um histograma e em uma nuvem de palavras
+
+## Célula de Cógido (1) - Geração de dados com nome e uma pontuação para cada pessoa
+
+## Célula de Código (2) - Gravar os dados em um arquivo texto
+
+## Célula de Código (3) - Recuperar os dados do arquivo texto
+
